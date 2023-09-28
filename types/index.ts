484d89c78de408150e15b1cec5d763db75cd69c0
William Giammona <wgiammona@gmail.com>
2023-09-28 17:29:18 +0300

    added btnType and name change

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,8 @@
 import { MouseEventHandler } from "react";
 
-export interface CustomButtionProps {
+export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
 }
