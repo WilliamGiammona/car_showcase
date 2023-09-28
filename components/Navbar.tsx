a0d1ce19edf3f040262e645e2ca7e82ea9e19614
William Giammona <wgiammona@gmail.com>
2023-09-28 15:03:26 +0300

    added imports

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+import Image from "next/image";
+import CustomButtion from "./CustomButtion";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
