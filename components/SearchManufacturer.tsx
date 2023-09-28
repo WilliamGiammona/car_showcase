ce68d28d697bae99f561340deeae72d786ea512f
William Giammona <wgiammona@gmail.com>
2023-09-28 21:08:10 +0300

    Adding headlessui Combobox and Transition

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
 
-const SearchManufacturer = () => {
-  return <div>SearchManufacturer</div>;
+import { SearchManufacturerProps } from "@/types";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { useState, Fragment } from "react";
+
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full h-[20px]">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              alt="Car Logo"
+              width={20}
+              height={20}
+              className="ml-4 "
+            />
+          </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string): any => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+          ></Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
